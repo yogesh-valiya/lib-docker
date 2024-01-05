cb1b9d529e8f0af5d16a497490e43b43b28ac166
Yogesh Valiya <yvaliya4@gmail.com>
2024-01-05 12:05:00 +0530

    Make docker patch dynamic

--- bin/restart.sh
+++ bin/restart.sh
@@ -3,6 +3,6 @@
 echo "Starting docker container..."
 for var in "$@"; do
   echo "Starting $var..."
-  /usr/bin/docker-compose -f /magento/dockers/docker/envs/$var/compose.yml down
-  /usr/bin/docker-compose -f /magento/dockers/docker/envs/$var/compose.yml up -d
+  /usr/bin/docker-compose -f $(dirname "$0")/../docker/envs/$var/compose.yml down
+  /usr/bin/docker-compose -f $(dirname "$0")/../docker/envs/$var/compose.yml up -d
 done
--- bin/start.sh
+++ bin/start.sh
@@ -3,5 +3,5 @@
 echo "Starting docker container..."
 for var in "$@"; do
   echo "Starting $var..."
-  /usr/bin/docker-compose -f /magento/dockers/docker/envs/$var/compose.yml up -d
+  /usr/bin/docker-compose -f $(dirname "$0")/../docker/envs/$var/compose.yml up -d
 done
